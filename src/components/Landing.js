708543ec53fc6c63f069b789a47ca5d44181093e
Jesse Maretz <jesse.maretz@gmail.com>
2020-12-06 15:26:06 -0500

    created the component folder and started the navbar and landing page class components

---
+++ src/components/Landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class Landing extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                <h1>This is the landing page</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Landing
