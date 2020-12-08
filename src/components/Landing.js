ed3e96dfff8e60727bc3098f1d5e52c7e32130c3
Jesse Maretz <jesse.maretz@gmail.com>
2020-12-08 18:26:34 -0500

    pushing gitignore

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar'
 
 
 class Landing extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            datacategories: []
+        }
     }
+
+
+
+
+    componentDidMount = () => {
+        let url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories"
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                //'Content-Type': 'application/json',
+                'token': process.env.REACT_APP_CLIMATE_TOKEN
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.state.datacategories = data
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+
+
     render(){
         return(
             <div>
+                <header>
+                    <Navbar />
+                </header>
                 <h1>This is the landing page</h1>
+                
             </div>
         )
     }
