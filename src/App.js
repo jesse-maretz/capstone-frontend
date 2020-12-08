12ca10836b67bb7e553fb6c195e87021d5388478
Jesse Maretz <jesse.maretz@gmail.com>
2020-12-07 20:31:19 -0500

    pushing commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Landing from './components/Landing'
+import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
+      <header>
+        <Navbar />
+      </header>
       <h1>Natural Data</h1>
       <Router>
         <Route exact path="/"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-
-
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+//import './../style/Navbar.css'
 
 class Navbar extends Component{
     constructor(props){
@@ -9,8 +8,8 @@ class Navbar extends Component{
     }
     render(){
         return(
-            <div>
-                
+            <div className='navbar'>
+                <h1>This is the navbar</h1>
             </div>
         )
     }
