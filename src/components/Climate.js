4ff06097b55d84967df300eff816f9f20445aee8
Jesse Maretz <jesse.maretz@gmail.com>
2020-12-08 21:30:05 -0500

    created more components

---
+++ src/components/Climate.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class Climate extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            datacategories: [],
+            locations: [],
+            locationcategories: [],
+            datasets: []
+        }
+    }
+
+
+
+    componentDidMount = () => {
+        /*
+        let dataCatUrl = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories"
+        fetch(dataCatUrl, {
+            method: 'GET',
+            headers: {
+                'token': process.env.REACT_APP_CLIMATE_TOKEN
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.state.datacategories = data.results
+                console.log(this.state.datacategories)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+            */
+        let locationsUrl = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations"
+        fetch(locationsUrl, {
+            method: 'GET',
+            headers: {
+                'token': process.env.REACT_APP_CLIMATE_TOKEN
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.state.locations = data.results
+                console.log(this.state.locations)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+/*
+        let locationCategoriesUrl = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locationcategories/CITY"
+        fetch(locationCategoriesUrl, {
+            method: 'GET',
+            headers: {
+                'token': process.env.REACT_APP_CLIMATE_TOKEN
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.state.locationcategories = data.results
+                console.log(this.state.locationcategories)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            */
+    }
+
+
+
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Climate;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,31 +5,6 @@ import Navbar from './Navbar'
 class Landing extends Component{
     constructor(props){
         super(props)
-        this.state = {
-            datacategories: []
-        }
-    }
-
-
-
-
-    componentDidMount = () => {
-        let url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories"
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                //'Content-Type': 'application/json',
-                'token': process.env.REACT_APP_CLIMATE_TOKEN
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                this.state.datacategories = data
-            })
-            .catch(err => {
-                console.log(err)
-            })
     }
 
 
