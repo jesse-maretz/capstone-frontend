8a5f5603d107e51f147160856a58e44a4e9f1208
Jesse Maretz <jesse.maretz@gmail.com>
2020-12-09 19:48:06 -0500

    added footprint input

--- src/components/Footprint.js
+++ src/components/Footprint.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Container, Button, InputLabel, Box, Input } from '@material-ui/core';
 import iframe from 'react-iframe' 
 import Navbar from './Navbar'
 
@@ -6,8 +7,14 @@ import Navbar from './Navbar'
 class Footprint extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            footprint: 0
+        }
     }
 
+    handleFootprint = (e) => {
+        this.setState({footprint: e.target.value})
+    }
 
 
     render(){
@@ -22,9 +29,18 @@ class Footprint extends Component{
                         title="Footprint Calculator"
                         width="1000"
                         height="1000"
-                        src="https://www.carbonfootprint.com/calculator.aspx">
+                        src="https://www3.epa.gov/carbon-footprint-calculator/">
                     </iframe>
-                </div>                
+                </div>
+                <Container>
+                    <InputLabel>Your footprint: </InputLabel>
+                    <Input
+                        required
+                        onChange={this.handleFootprint}
+                        type="number"
+                        placeholder="my footprint is...">
+                    </Input>
+                </Container>
             </div>
         )
     }
